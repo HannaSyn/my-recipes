f0390ea30d9d743831354c52cd74b5db6c3e770c
Hanna Syniavska <hanna.syniavska@qpard.com>
2022-05-12 18:53:24 +0200

    get data from firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import { useState, useEffect } from "react";
 import { db } from "./firebase.confiig";
+import { collection, onSnapshot } from "firebase/firestore";
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [form, setForm] = useState({
+    title: '',
+    description: '',
+    ingridients: [],
+    steps: [],
+  });
+
+  const recipesCollectionRef = collection(db, 'recipes');
+
+  useEffect(() => {
+    onSnapshot(recipesCollectionRef, snapshot => {
+      setRecipes(snapshot.docs.map(doc => {
+        return {
+          id: doc.id,
+          viewing: false,
+          ...doc.data()
+        }
+      }))
+    })
+  }, []);
+
+  console.log(recipes);
+  
   return (
     <div className="App">
     </div>
